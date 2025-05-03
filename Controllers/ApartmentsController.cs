f2cdc6da9b06a87faf773486bca45f329c261e5d
Svet-39 <svetoslavakostova321@abv.bg>
2025-05-03 17:25:46 +0300

    add apartments functionality

---
+++ Controllers/ApartmentsController.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using RentalAppMVC.Data;
+
+namespace RentalAppMVC.Controllers
+{
+    public class ApartmentsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ApartmentsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Apartments
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Apartments.Include(a => a.User);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Apartments/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return View(apartment);
+        }
+
+        // GET: Apartments/Create
+        public IActionResult Create()
+        {
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id");
+            return View();
+        }
+
+        // POST: Apartments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Type,FloorNumber,HasElevator,Id,Title,Description,Price,Address,ImageUrl,UserId")] Apartment apartment)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(apartment);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", apartment.UserId);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments.FindAsync(id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", apartment.UserId);
+            return View(apartment);
+        }
+
+        // POST: Apartments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Type,FloorNumber,HasElevator,Id,Title,Description,Price,Address,ImageUrl,UserId")] Apartment apartment)
+        {
+            if (id != apartment.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(apartment);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ApartmentExists(apartment.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["UserId"] = new SelectList(_context.Set<User>(), "Id", "Id", apartment.UserId);
+            return View(apartment);
+        }
+
+        // GET: Apartments/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var apartment = await _context.Apartments
+                .Include(a => a.User)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (apartment == null)
+            {
+                return NotFound();
+            }
+
+            return View(apartment);
+        }
+
+        // POST: Apartments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var apartment = await _context.Apartments.FindAsync(id);
+            if (apartment != null)
+            {
+                _context.Apartments.Remove(apartment);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ApartmentExists(int id)
+        {
+            return _context.Apartments.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Data/Migrations/20250503141108_UserPropertiesSchemas.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentalAppMVC.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UserPropertiesSchemas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Discriminator",
+                table: "AspNetUsers",
+                type: "nvarchar(13)",
+                maxLength: 13,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "Property",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ImageUrl = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Discriminator = table.Column<string>(type: "nvarchar(13)", maxLength: 13, nullable: false),
+                    FloorNumber = table.Column<int>(type: "int", nullable: true),
+                    HasElevator = table.Column<bool>(type: "bit", nullable: true),
+                    NumberOfRooms = table.Column<int>(type: "int", nullable: true),
+                    HasGarage = table.Column<bool>(type: "bit", nullable: true),
+                    HasBalcony = table.Column<bool>(type: "bit", nullable: true),
+                    Floors = table.Column<int>(type: "int", nullable: true),
+                    SizeInSquareMeters = table.Column<double>(type: "float", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Property", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Property_AspNetUsers_UserId",
+                        column: x => x.UserId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Property_UserId",
+                table: "Property",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Property");
+
+            migrationBuilder.DropColumn(
+                name: "Discriminator",
+                table: "AspNetUsers");
+        }
+    }
+}
