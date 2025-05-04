8952b7e77f6972df93c33be16207d807c8adec6f
eon <dobrin_k1@abv.bg>
2025-05-04 14:41:21 +0300

    add apartment service

---
+++ Services/Abstractions/IApartmentService.cs
@@ -0,0 +1,14 @@
+﻿using RentalAppMVC.DTOs;
+
+namespace RentalAppMVC.Services.Abstractions
+{
+    public interface IApartmentService
+    {
+        Task<List<ApartmentDTO>> GetAllAsync();
+        Task<ApartmentDTO> GetByIdAsync(int id);
+        Task<List<ApartmentDTO>> GetApartmentByAddressAsync(string address);
+        Task AddAsync(ApartmentDTO model);
+        Task UpdateAsync(ApartmentDTO model);
+        Task DeleteByIdAsync(int id);
+    }
+}
