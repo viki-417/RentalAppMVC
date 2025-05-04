21bdbfaab8206e1afc8f6ae9cc8a14d87039d45a
eon <dobrin_k1@abv.bg>
2025-05-04 14:41:03 +0300

    add crud repository

--- Data/Property.cs
+++ Data/Property.cs
@@ -1,8 +1,7 @@
 ﻿namespace RentalAppMVC.Data
 {
-    public abstract class Property
+    public abstract class Property : BaseEntity
     {
-        public int Id { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public double Price { get; set; }
---
+++ Repositories/CrudRepository.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentalAppMVC.Data;
+using RentalAppMVC.Repositories.Abstractions;
+using System.Linq.Expressions;
+
+namespace RentalAppMVC.Repositories
+{
+    public class CrudRepository<T> : ICrudRepository<T>
+       where T : BaseEntity
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public CrudRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteByIdAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<IEnumerable<T>> GetByFilterAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await _dbSet
+                .Where(predicate)
+                .ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            _context.Entry(entity).State = EntityState.Modified;
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
