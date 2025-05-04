a71ed6a656fd04ea1d2958d22c329abf28c6a908
viki-417 <ivanoviktoriq07@gmail.com>
2025-05-04 14:56:32 +0300

    add dtos and mapping profiles

---
+++ Profiles/ApartmentProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RentalAppMVC.Data;
+using RentalAppMVC.DTOs;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace RentalAppMVC.Profiles
+{
+    public class ApartmentProfile : Profile
+    {
+        public ApartmentProfile()
+        {
+            CreateMap<Apartment, ApartmentDTO>()
+                .ReverseMap();
+        }
+    }
+}
