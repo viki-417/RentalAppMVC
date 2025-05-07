7429e5dad85b25e8a2109f49a0be0580b5f37e1d
viki-417 <ivanoviktoriq07@gmail.com>
2025-05-07 22:38:46 +0300

    Add PropertyDTO and UserDTO with IsAvailable field

--- DTOs/UserDTO.cs
+++ DTOs/UserDTO.cs
@@ -2,7 +2,7 @@
 {
     public class UserDTO
     {
-        public string Id { get; set; }
-        public string Email { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
     }
 }
