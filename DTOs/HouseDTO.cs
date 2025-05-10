8dd6c72b7ce375353d91f52987df35357ea9ee00
Svet-39 <svetoslavakostova321@abv.bg>
2025-05-10 11:07:29 +0300

    add minor changes

--- DTOs/HouseDTO.cs
+++ DTOs/HouseDTO.cs
@@ -1,19 +1,7 @@
 ﻿namespace RentalAppMVC.DTOs
 {
-    public class HouseDTO
+    public class HouseDTO : PropertyDTO
     {
-        public int Id { get; set; }
-
-        public string Title { get; set; }
-
-        public string Address { get; set; }
-
-        public decimal Price { get; set; }
-
-        public string Description { get; set; }
-
-        public string UserId { get; set; }
-
         public int NumberOfRooms { get; set; }
         public bool HasGarage { get; set; }
         public bool HasBalcony { get; set; }
