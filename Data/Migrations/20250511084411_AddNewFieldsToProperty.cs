c97a7143e8da741bbf53630f807ceb489da688f6
PC-17 <vbzala141@gmail.com>
2025-05-11 11:44:52 +0300

    add migrations

---
+++ Data/Migrations/20250511084411_AddNewFieldsToProperty.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RentalAppMVC.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewFieldsToProperty : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "AC",
+                table: "Properties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Balcony",
+                table: "Properties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Furnitured",
+                table: "Properties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Garage",
+                table: "Properties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Tv",
+                table: "Properties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "Wifi",
+                table: "Properties",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AC",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Balcony",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Furnitured",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Garage",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Tv",
+                table: "Properties");
+
+            migrationBuilder.DropColumn(
+                name: "Wifi",
+                table: "Properties");
+        }
+    }
+}
